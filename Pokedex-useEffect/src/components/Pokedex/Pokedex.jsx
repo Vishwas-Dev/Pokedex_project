1da29314fafa1e1ee9293d18714479202006fe88
Vishwas <122880819+Vishwas-Dev@users.noreply.github.com>
2026-04-30 21:21:09 +0530

    Implement search functionality in Pokedex component

--- Pokedex-useEffect/src/components/Pokedex/Pokedex.jsx
+++ Pokedex-useEffect/src/components/Pokedex/Pokedex.jsx
@@ -5,6 +5,17 @@ import PokemonDetails from '../PokemonDetails/PokemonDetails'
 // Css
 import './Pokedex.css'
 
+  function Pokedex() {
+  const [seacrhTerm, setSearchTerm] = useState('')
+  return (
+    <div className='pokedex-wrapper'>
+      
+      <Search updateSearchTerm={setSearchTerm} />
+      {( !seacrhTerm) ? <PokemonList/> : <PokemonDetails key={seacrhTerm} pokemonName={seacrhTerm}/> }
+    </div>
+  )
+}
+
 
 
 export default Pokedex
