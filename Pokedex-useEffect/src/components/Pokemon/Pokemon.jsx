8acbc2d09e11ef13be093eaffae2a1070e1a564f
Vishwas <122880819+Vishwas-Dev@users.noreply.github.com>
2026-04-30 21:22:28 +0530

    Implement Pokemon component with details and link
    Added a functional component to display Pokémon details.

--- Pokedex-useEffect/src/components/Pokemon/Pokemon.jsx
+++ Pokedex-useEffect/src/components/Pokemon/Pokemon.jsx
@@ -2,6 +2,18 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import './Pokemon.css'
 
-
+function Pokemon({name, image, id}) {
+  return (
+    <div className='pokemon'>
+      <Link to={`/pokemon/${id}`}>
+      <div className='pokemon-name'><span>{name}</span></div>
+     <div>
+      <img className='pokemon-img' src={image} alt="pokemons-images/avatar" />
+      </div>  
+      </Link>
+     
+    </div>
+  )
+}
 
 export default Pokemon
